37bc293c54ceda73119c7d90a155c1366def25e4
twopill <ceravolo.elia@gmail.com>
2021-02-20 19:22:55 +0100

    fix code

--- searchInfo.py
+++ searchInfo.py
@@ -1,26 +1,41 @@
 import tweepy
-import sys
 import csv
+from datetime import datetime
+import datetime
 
-from twitter_app_credentials import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secrete)
+
 api = tweepy.API(auth)
-                                          #al posto di "covid" metti la parola che ti interessa cercare
-public_tweets = tweepy.Cursor(api.search, q="codiv", result_type="mixed", tweet_mode="extended").items(numeroditweetchevuoi)
+tweets_for_csv = []
+                                         #="parametro da cercare"                                          #numero di tweet ( Massimo numero = 100 ) 
+public_tweets = tweepy.Cursor(api.search, q="covid", result_type="mixed", tweet_mode="extended", lang='it').items(1)
 
+d = datetime.datetime.now()
+def noZeroDay(hour, minute, second):
+    # method for add 0 if hour || minute == 1
+    h = ''
+    m = ''
+    if len(str(hour)) == 1:
+        h = str( '0' + hour )
+    elif len(str(hour)) > 1: 
+        h = str(hour)
+    if len(str(minute)) == 1:
+        m = str( '0' + minute )
+    elif len(str(minute)) > 1:
+        m = str(minute)
+    return str(h) + '-' + str(m) + '-' + str(second)
+
+timestamp = str(datetime.date.today()) + '--' + noZeroDay(d.hour,d.minute,d.second)
+
+# for all tweet, push them in the array for create after a csv file
 for tweet in public_tweets:
-  print("@"tweet.user.screen_name
-  tweet_text = tweet.full_text)
+  tweets_for_csv.append([tweet.full_text])
   
-#non toccare
-
-		tweets_for_csv.append([username, tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")])
-
-	
-	  outfile = username + "_tweets.csv"
-	  print "writing to " + outfile
-	  with open(outfile, 'w+') as file:
-		writer = csv.writer(file, delimiter=',')
-		writer.writerows(tweets_for_csv)
+# create the name of the file, example: 2020-10-20--18-22-33_tweets.csv
+outfile = timestamp + "_tweets.csv"
+with open(outfile, 'w+', encoding='utf-8') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(['COMMENTI,'])
+    writer.writerows(tweets_for_csv)
